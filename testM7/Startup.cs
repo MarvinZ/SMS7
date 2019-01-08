97a49a2c827fcbda778da3ce96fe5ce3a915d920
Marvin Zumbado <marvinzzz@gmail.com>
2019-01-07 20:07:29 -0600

    Add project files.

---
+++ testM7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testM7.Startup))]
+namespace testM7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
